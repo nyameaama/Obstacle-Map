dfa07c10e253bbc12a98653d4cf02ee3704b1024
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-07-06 21:31:55 +0000

    Additions

---
+++ src/2D/Map2D.h
@@ -0,0 +1,11 @@
+#ifndef MAP_2D 
+#define MAP_2D
+
+class MAP2D {
+    public:
+
+
+
+};
+
+#endif
--- src/ObstacleMap.h
+++ src/ObstacleMap.h
@@ -1,6 +1,27 @@
 #ifndef MAIN_
 #define MAIN_
 
+#include<stdint.h>
+#include<stddef.h>
+#include<string.h>
 
+#define INIT (uint8_t) 1
+
+template<typename TYPEDEF>
+class OBSTACLEMAP {
+    public:
+        //Param = INIT token or custom specifier ID 
+        uint8_t MAP2D(TYPEDEF TOKEN);
+
+        //Param = INIT token or custom specifier ID 
+        uint8_t MAP3D(TYPEDEF TOKEN);
+
+        void ADD_TO_MAP_2D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value);
+
+        void ADD_TO_MAP_3D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value,TYPEDEF ALTITUDE);
+
+        //Returns file ID <char type>
+        char* OUTPUT_TO_JSON(TYPEDEF TOKEN);
+};
 
 #endif
