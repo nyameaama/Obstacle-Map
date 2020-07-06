c15bdbbd0d49469f95885d016ceec4b5ffdee2d7
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-07-06 21:36:28 +0000

    Fixed Template issues

---
+++ src/ObstacleMap.cpp
@@ -0,0 +1,26 @@
+#include"ObstacleMap.h"
+
+template<typename TYPEDEF>
+ //Param = INIT token or custom specifier ID 
+uint8_t OBSTACLEMAP::MAP2D(TYPEDEF TOKEN){
+
+}
+
+template<typename TYPEDEF>
+//Param = INIT token or custom specifier ID 
+uint8_t OBSTACLEMAP::MAP3D(TYPEDEF TOKEN){
+
+}
+template<typename TYPEDEF>
+void OBSTACLEMAP::ADD_TO_MAP_2D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value){
+
+}
+template<typename TYPEDEF>
+void OBSTACLEMAP::ADD_TO_MAP_3D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value,TYPEDEF ALTITUDE){
+
+}
+template<typename TYPEDEF>
+//Returns file ID <char type>
+char* OBSTACLEMAP::OUTPUT_TO_JSON(TYPEDEF TOKEN){
+
+}
--- src/ObstacleMap.h
+++ src/ObstacleMap.h
@@ -7,19 +7,23 @@
 
 #define INIT (uint8_t) 1
 
-template<typename TYPEDEF>
 class OBSTACLEMAP {
     public:
+        template<typename TYPEDEF>
         //Param = INIT token or custom specifier ID 
         uint8_t MAP2D(TYPEDEF TOKEN);
 
+        template<typename TYPEDEF>
         //Param = INIT token or custom specifier ID 
         uint8_t MAP3D(TYPEDEF TOKEN);
 
+        template<typename TYPEDEF>
         void ADD_TO_MAP_2D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value);
 
+        template<typename TYPEDEF>
         void ADD_TO_MAP_3D(uint8_t MAP_ID,TYPEDEF bearing, TYPEDEF value,TYPEDEF ALTITUDE);
 
+        template<typename TYPEDEF>
         //Returns file ID <char type>
         char* OUTPUT_TO_JSON(TYPEDEF TOKEN);
 };
